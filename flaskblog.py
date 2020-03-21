2a87fbe36deec9d191745193bbf551fd7bb28d7f
Valmir Ferreira <valmirf2n@gmail.com>
2020-03-21 18:41:42 -0300

    Created registration and login forms and their validations

--- flaskblog.py
+++ flaskblog.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '999cdc73be3f423bba567b0a4c12ea62a1276f4a010d5fa9961728a46e170ae8'
 
 # Dummy variables to use in html
 posts = [
@@ -25,3 +27,26 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html', title='About')
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been loged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+
+    return render_template('login.html', title='Login', form=form)
