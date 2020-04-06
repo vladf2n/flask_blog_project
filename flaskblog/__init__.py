38a6347f126752aa9ce6d2f087c7a9e8c612a65d
Valmir Ferreira <valmirf2n@gmail.com>
2020-04-06 20:56:09 -0300

    User Authentication is working now

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,9 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '999cdc73be3f423bba567b0a4c12ea62a1276f4a010d5fa9961728a46e170ae8'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
 from flaskblog import routes
--- flaskblog/models.py
+++ flaskblog/models.py
@@ -1,9 +1,16 @@
 from datetime import datetime
 # flaskblog is the package containing all of my application but the run file
-from flaskblog import db
+from flaskblog import db, login_manager
+from flask_login import UserMixin
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
 
 # Each Class is going to be a Model in the Database
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
